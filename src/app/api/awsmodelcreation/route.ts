f44b4f803cb5f76b181073461e9eda5697a14a5b
wangjiaqi1105 <jiaqiwang1105@hotmail.com>
2025-05-21 09:24:50 -0700

    backend insCreation done

--- src/app/api/awsmodelcreation/route.ts
+++ src/app/api/awsmodelcreation/route.ts
@@ -2,20 +2,27 @@ import { EC2Client, DescribeInstancesCommand } from '@aws-sdk/client-ec2';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function POST(req: NextRequest) {
-  const { accessKey, secretKey, region } = await req.json();
+  const { accessKey, secretKey } = await req.json();
 
-  if (!accessKey || !secretKey || !region) {
+  // const { accessKey, secretKey, region } = await req.json();
+  //! region harded; need to be convered later
+
+  if (!accessKey || !secretKey) {
+    //if (!accessKey || !secretKey || !region) {
     //! need to sync up with frontend for namings
+    //! need to add !region back , curretnly hard-coded
 
     return NextResponse.json(
-      { error: 'Missing AWS credentials or region' },
+      { error: 'Missing AWS AccessKey or SecretKey' },
       { status: 400 }
     );
   }
 
   try {
     const ec2 = new EC2Client({
-      region,
+      //region:'us-west-1',
+      region: 'us-west-1',
+      //! region harded; need to be convered later
       credentials: {
         accessKeyId: accessKey,
         secretAccessKey: secretKey,
@@ -25,9 +32,28 @@ export async function POST(req: NextRequest) {
     const command = new DescribeInstancesCommand({});
     const result = await ec2.send(command);
 
-    const ins = result.Reservations?.flatMap((el) => el.Instances) || [];
-    console.log('TEST!!!!!!!', ins);
-    return NextResponse.json({ ins });
+    const instances = result.Reservations?.flatMap((el) => el.Instances);
+    // console.log('👀 👀 👀 👀 TEST!!!!!!!', instances);
+    //console.log(JSON.stringify(instances, null, 2));
+
+    const res = instances?.map((el) => {
+      return {
+        instanceId: el?.InstanceId,
+        state: el?.State?.Name,
+        name: el?.Tags?.find((tag) => tag.Key === 'Name')?.Value,
+        type: el?.InstanceType,
+        launchTime: el?.LaunchTime,
+        SecurityGroups: el?.SecurityGroups?.map((el) => ({
+          groupId: el.GroupId,
+          groupName: el.GroupName,
+        })),
+        PrivateIpAddress: el?.PrivateIpAddress,
+        PublicIpAddress: el?.PublicIpAddress,
+      };
+    });
+    console.log('👀 👀 👀 👀 TEST!!!!!!!', res);
+
+    return NextResponse.json({ res }, { status: 200 });
   } catch (err) {
     console.error('❌ AWS Error:', err);
     return NextResponse.json(
