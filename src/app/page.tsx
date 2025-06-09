afb72d81195ac15e4822f6bf9fd35a4626d1af72
FengJie <fllying8791@gmail.com>
2025-06-09 11:30:31 -0700

    Deleted Link and Hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,4 @@
-import Link from 'next/link';
-
 import LandingClientWapper from '../../components/landing/LandingClientWapper';
-import Hero from '../../components/landing/Hero';
 export default function Home() {
   return (
     <section>
