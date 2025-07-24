903631f224744ab134616d85c9c53a65ff7d8122
CarlborHal <grwlp5k@gmail.com>
2025-07-24 19:39:19 -0400

    Finalised K8 ELB

--- src/app/api/metriccompute/route.ts
+++ src/app/api/metriccompute/route.ts
@@ -1,11 +1,8 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function POST(req: NextRequest) {
-  // Extract credentials and region from the request body
   const data = await req.json();
-  // console.log('data', data.instanceMetrics)
   const getAnalysis = await fetch('http://54.234.40.140//python/analysis', {
-    //move to backend, then make backend connect
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
@@ -14,8 +11,7 @@ export async function POST(req: NextRequest) {
       data: data.instanceMetrics,
     }),
   });
-  const res = await getAnalysis.json()
+  const res = await getAnalysis.json();
 
-
-  return NextResponse.json({ res:res }, { status: 200 });
+  return NextResponse.json({ res: res }, { status: 200 });
 }
